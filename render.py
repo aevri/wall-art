ddc305c52a93af3a86fa19c9701e2ab1992a5ba6
Angelos Evripiotis <aevri@users.noreply.github.com>
2023-11-25 21:04:22 +0000

    Color gradient on X axis

--- render.py
+++ render.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 
 width = 800
 height = 800
-canvas = np.ones((width, height, 3))
+canvas = np.ones((height, width, 3))
 
-lime_green = np.array([0.196, 0.804, 0.196])
-forest_green = np.array([0.133, 0.545, 0.133])
-spring_green = np.array([0.000, 1.000, 0.498])
-sea_green = np.array([0.180, 0.545, 0.341])
+x = np.linspace(0, 1, width).reshape(1, width, 1)
 
-canvas *= sea_green
+lime_green = np.array([[0.196, 0.804, 0.196]])
+forest_green = np.array([[0.133, 0.545, 0.133]])
+spring_green = np.array([[0.000, 1.000, 0.498]])
+sea_green = np.array([[0.180, 0.545, 0.341]])
+
+canvas *= sea_green * x + forest_green * (1 - x)
 
 # Save the image as "out.png"
 canvas = (canvas * 255).astype(np.uint8)
