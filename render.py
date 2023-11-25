cbeab4d927f30b297ba0b9a3c062795bee4416d9
Angelos Evripiotis <aevri@users.noreply.github.com>
2023-11-25 21:45:25 +0000

    draw white, blurred circles, inefficiently

--- render.py
+++ render.py
@@ -1,8 +1,10 @@
 import numpy as np
+from scipy.ndimage import gaussian_filter
 import matplotlib.pyplot as plt
 
-width = 800
-height = 800
+width = 2000
+height = 2000
+mid_y = height // 2
 canvas = np.ones((height, width, 3))
 
 x = np.linspace(0, 1, width).reshape(1, width, 1)
@@ -10,11 +12,29 @@ y = np.linspace(0, 1, height).reshape(height, 1, 1)
 
 lime_green = np.array([[0.196, 0.804, 0.196]])
 forest_green = np.array([[0.133, 0.545, 0.133]])
-spring_green = np.array([[0.000, 1.000, 0.498]])
 sea_green = np.array([[0.180, 0.545, 0.341]])
+corn_flour_white = np.array([[0.961, 0.961, 0.863]])
 
 canvas *= sea_green * x + forest_green * (1 - x)
 canvas = lime_green * y + canvas * (1 - y)
+
+def draw_circle(center, radius, color):
+    x_coords, y_coords = np.meshgrid(np.arange(width), np.arange(height))
+    distances = np.sqrt((x_coords - center[0])**2 + (y_coords - center[1])**2)
+    mask = distances <= radius
+    canvas[mask] = color
+
+draw_circle((100, mid_y), 10, corn_flour_white)
+draw_circle((200, mid_y), 20, corn_flour_white)
+draw_circle((300, mid_y), 30, corn_flour_white)
+draw_circle((500, mid_y), 40, corn_flour_white)
+draw_circle((800, mid_y), 50, corn_flour_white)
+draw_circle((1300, mid_y), 60, corn_flour_white)
+draw_circle((2100, mid_y), 70, corn_flour_white)
+
+# Apply Gaussian filter to each color channel separately
+for i in range(3):
+    canvas[:, :, i] = gaussian_filter(canvas[:, :, i], sigma=16)
 
 # Save the image as "out.png"
 canvas = (canvas * 255).astype(np.uint8)
