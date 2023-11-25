7d134c5aebbcf4a6d3a80480ef2c939a219d6232
Angelos Evripiotis <aevri@users.noreply.github.com>
2023-11-25 20:57:28 +0000

    solid green background

--- render.py
+++ render.py
@@ -1,8 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Create a white square image
-image = np.ones((800, 800, 3), dtype=np.uint8) * 255
+width = 800
+height = 800
+canvas = np.ones((width, height, 3))
+
+lime_green = np.array([0.196, 0.804, 0.196])
+forest_green = np.array([0.133, 0.545, 0.133])
+spring_green = np.array([0.000, 1.000, 0.498])
+sea_green = np.array([0.180, 0.545, 0.341])
+
+canvas *= sea_green
 
 # Save the image as "out.png"
-plt.imsave("out.png", image)
+canvas = (canvas * 255).astype(np.uint8)
+plt.imsave("out.png", canvas)
