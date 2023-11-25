6fd70a565e8582918bb2027eee4a226339354d32
Angelos Evripiotis <aevri@users.noreply.github.com>
2023-11-25 21:06:54 +0000

    Color gradient on Y axis

--- render.py
+++ render.py
@@ -6,6 +6,7 @@ height = 800
 canvas = np.ones((height, width, 3))
 
 x = np.linspace(0, 1, width).reshape(1, width, 1)
+y = np.linspace(0, 1, height).reshape(height, 1, 1)
 
 lime_green = np.array([[0.196, 0.804, 0.196]])
 forest_green = np.array([[0.133, 0.545, 0.133]])
@@ -13,6 +14,7 @@ spring_green = np.array([[0.000, 1.000, 0.498]])
 sea_green = np.array([[0.180, 0.545, 0.341]])
 
 canvas *= sea_green * x + forest_green * (1 - x)
+canvas = lime_green * y + canvas * (1 - y)
 
 # Save the image as "out.png"
 canvas = (canvas * 255).astype(np.uint8)
