6b8ff5b280ba7599d964705841893d7104eb9df9
Angelos Evripiotis <aevri@users.noreply.github.com>
2023-11-26 21:02:50 +0000

    RGBA

--- render.py
+++ render.py
@@ -16,23 +16,20 @@ def main():
     width = 800
     height = 800
     mid_y = height // 2
-    canvas = np.ones((height, width, 3))
+    canvas = np.ones((height, width, 4))  # RGBA
 
     x = np.linspace(0, 1, width).reshape(1, width, 1)
     y = np.linspace(0, 1, height).reshape(height, 1, 1)
 
-    #lime_green = np.array([[0.196, 0.804, 0.196]])
-    #forest_green = np.array([[0.133, 0.545, 0.133]])
-    #sea_green = np.array([[0.180, 0.545, 0.341]])
-    eucalyptus_green = np.array([[0.392, 0.706, 0.588]])
-    eucalyptus_green2 = np.array([[0.412, 0.706, 0.588]])
-    eucalyptus_green3 = np.array([[0.372, 0.706, 0.588]])
-    eucalyptus_green4 = np.array([[0.392, 0.726, 0.588]])
-    eucalyptus_green5 = np.array([[0.392, 0.706, 0.608]])
-    eucalyptus_green6 = np.array([[0.392, 0.686, 0.588]])
+    eucalyptus_green = np.array([[0.392, 0.706, 0.588, 1]])
+    eucalyptus_green2 = np.array([[0.412, 0.706, 0.588, 1]])
+    eucalyptus_green3 = np.array([[0.372, 0.706, 0.588, 1]])
+    eucalyptus_green4 = np.array([[0.392, 0.726, 0.588, 1]])
+    eucalyptus_green5 = np.array([[0.392, 0.706, 0.608, 1]])
+    eucalyptus_green6 = np.array([[0.392, 0.686, 0.588, 1]])
 
-    sage_green = np.array([[0.557, 0.663, 0.557]])
-    racing_green = np.array([[0.0, 0.5, 0.0]])
+    sage_green = np.array([[0.557, 0.663, 0.557, 1]])
+    racing_green = np.array([[0.0, 0.5, 0.0, 1]])
 
     color_scale = np.linspace(racing_green, eucalyptus_green5, 100)
 
@@ -45,8 +42,6 @@ def main():
         radius = random.randint(10, mid_y * 0.25)
         center_x = random.randint(0, width)
         center_y = random.randint(0, height)
-        color = np.random.rand(1, 3)
-        color[0, 1] = 0.5 * color[0, 1] + 0.5
         color = random.choice(color_scale)
         draw_circle(canvas, (center_x, center_y), radius, color)
 
